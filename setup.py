efcb734cc448a31d915e642505a41d499e0433d5
Aaron Graubert <captianjroot@live.com>
2018-08-31 10:08:13 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'lapdog',
-    version = '0.0.7',
+    version = '0.1.0',
     packages = ['lapdog'],
     package_data={'':['wdl_pipeline.yaml']},
     description = 'A wrapper for FISS and dalmatian',
