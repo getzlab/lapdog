a24bee489e55c0f9f3e56d7b4494a7b3c64ffb88
Aaron Graubert <captianjroot@live.com>
2019-03-29 15:42:41 -0400

    Added warning filter to silence application default warning

--- lapdog/__init__.py
+++ lapdog/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.filterwarnings('ignore', 'Your application has authenticated using end user credentials from Google Cloud SDK. We recommend that most server applications use service accounts instead. If your application continues to use end user credentials from Cloud SDK, you might receive a "quota exceeded" or "API not enabled" error. For more information about service accounts, see https://cloud.google.com/docs/authentication/')
 from dalmatian import *
 from .lapdog import *
 __version__ = "0.15.5"
