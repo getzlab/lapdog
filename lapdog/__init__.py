19c771a0fbc184f38fe6cc6a6cd7cbd391c1d4dd
Aaron Graubert <captianjroot@live.com>
2019-06-10 17:15:52 -0400

    Bump version to 0.17.0

--- lapdog/__init__.py
+++ lapdog/__init__.py
@@ -2,4 +2,4 @@ import warnings
 warnings.filterwarnings('ignore', 'Your application has authenticated using end user credentials from Google Cloud SDK. We recommend that most server applications use service accounts instead. If your application continues to use end user credentials from Cloud SDK, you might receive a "quota exceeded" or "API not enabled" error. For more information about service accounts, see https://cloud.google.com/docs/authentication/')
 from dalmatian import *
 from .lapdog import *
-__version__ = "0.16.4"
+__version__ = "0.17.0"
